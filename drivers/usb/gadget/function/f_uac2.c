c584a358b641edb77d580072d9557a3d1074bf92
Ronaldi <48817684+zephyrxj@users.noreply.github.com>
2019-07-06 15:01:11 +0700

    Update f_uac2.c

--- drivers/usb/gadget/function/f_uac2.c
+++ drivers/usb/gadget/function/f_uac2.c
@@ -1987,22 +1987,6 @@ in_rq_range(struct usb_function *fn, const struct usb_ctrlrequest *cr)
 
 	pr_debug("%s: entity_id:%u\n", __func__, entity_id);
 	if (control_selector == UAC2_CS_CONTROL_SAM_FREQ) {
-<<<<<<< HEAD
-		if (entity_id == USB_IN_CLK_ID || USB_OUT_CLK_ID) {
-			int i;
-
-			r.wNumSubRanges = CLK_FREQ_ARR_SIZE;
-			for (i = 0; i < CLK_FREQ_ARR_SIZE; i++) {
-				r.dRangeAttrs[i][0] = clk_frequencies[i];
-				r.dRangeAttrs[i][1] = r.dRangeAttrs[i][0];
-				r.dRangeAttrs[i][2] = 0;
-			}
-			value = min_t(unsigned, w_length, sizeof(r));
-			memcpy(req->buf, &r, value);
-		} else
-			return -EOPNOTSUPP;
-
-=======
 		if (entity_id == USB_IN_CLK_ID)
 			r.dMIN = cpu_to_le32(p_srate);
 		else if (entity_id == USB_OUT_CLK_ID)
@@ -2016,7 +2000,6 @@ in_rq_range(struct usb_function *fn, const struct usb_ctrlrequest *cr)
 
 		value = min_t(unsigned, w_length, sizeof r);
 		memcpy(req->buf, &r, value);
->>>>>>> v3.18.121
 	} else {
 		dev_err(&uac2->pdev.dev,
 			"%s:%d control_selector=%d TODO!\n",
