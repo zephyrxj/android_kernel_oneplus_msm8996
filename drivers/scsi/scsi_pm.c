7e200fc8b173f1b4bf773b2cce45a5376f3a909d
Ronaldi <48817684+zephyrxj@users.noreply.github.com>
2019-07-07 14:06:22 +0700

    Update scsi_pm.c

--- drivers/scsi/scsi_pm.c
+++ drivers/scsi/scsi_pm.c
@@ -250,25 +250,10 @@ static int sdev_runtime_suspend(struct device *dev)
 	struct scsi_device *sdev = to_scsi_device(dev);
 	int err = 0;
 
-<<<<<<< HEAD
-	if (!sdev->request_queue->dev) {
-		err = scsi_dev_type_suspend(dev, do_scsi_runtime_suspend);
-		if (err == -EAGAIN)
-			pm_schedule_suspend(dev, jiffies_to_msecs(
-					round_jiffies_up_relative(HZ/10)));
-		return err;
-	}
-
-	if (pm && pm->runtime_suspend) {
-		err = blk_pre_runtime_suspend(sdev->request_queue);
-		if (err)
-			return err;
-=======
 	err = blk_pre_runtime_suspend(sdev->request_queue);
 	if (err)
 		return err;
 	if (pm && pm->runtime_suspend)
->>>>>>> 1c857dc0e69f... Revert "SCSI: Fix NULL pointer dereference in runtime PM"
 		err = pm->runtime_suspend(dev);
 	blk_post_runtime_suspend(sdev->request_queue, err);
 
@@ -294,16 +279,8 @@ static int sdev_runtime_resume(struct device *dev)
 	const struct dev_pm_ops *pm = dev->driver ? dev->driver->pm : NULL;
 	int err = 0;
 
-<<<<<<< HEAD
-	if (!sdev->request_queue->dev)
-		return scsi_dev_type_resume(dev, do_scsi_runtime_resume);
-
-	if (pm && pm->runtime_resume) {
-		blk_pre_runtime_resume(sdev->request_queue);
-=======
 	blk_pre_runtime_resume(sdev->request_queue);
 	if (pm && pm->runtime_resume)
->>>>>>> 1c857dc0e69f... Revert "SCSI: Fix NULL pointer dereference in runtime PM"
 		err = pm->runtime_resume(dev);
 	blk_post_runtime_resume(sdev->request_queue, err);
 
