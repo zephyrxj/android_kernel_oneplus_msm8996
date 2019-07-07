9c446657970036564e686e3b1e1003327481c912
Ronaldi <48817684+zephyrxj@users.noreply.github.com>
2019-07-07 14:44:30 +0700

    Update quirks.c

--- drivers/usb/core/quirks.c
+++ drivers/usb/core/quirks.c
@@ -169,10 +169,6 @@ static const struct usb_device_id usb_quirk_list[] = {
 	{ USB_DEVICE(0x1908, 0x1315), .driver_info =
 			USB_QUIRK_HONOR_BNUMINTERFACES },
 
-<<<<<<< HEAD
-	/* INTEL VALUE SSD */
-	{ USB_DEVICE(0x8086, 0xf1a5), .driver_info = USB_QUIRK_RESET_RESUME },
-=======
 	/* Protocol and OTG Electrical Test Device */
 	{ USB_DEVICE(0x1a0a, 0x0200), .driver_info =
 			USB_QUIRK_LINEAR_UFRAME_INTR_BINTERVAL },
@@ -200,7 +196,6 @@ static const struct usb_device_id usb_quirk_list[] = {
 
 	/* Blackmagic Design Intensity Shuttle */
 	{ USB_DEVICE(0x1edb, 0xbd3b), .driver_info = USB_QUIRK_NO_LPM },
->>>>>>> 7f1643f1daf0... usb: quirks: Add delay-init quirk for Corsair K70 LUX RGB
 
 	/* USB3503 */
 	{ USB_DEVICE(0x0424, 0x3503), .driver_info = USB_QUIRK_RESET_RESUME },
